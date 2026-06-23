0609fb779c661d32c04326366008eaeacb00f62c
Tradehub-design <2gn2d7cmnf@privaterelay.appleid.com>
2026-06-24 02:44:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-APP_NAME = "TradeHub"
-BASE_CURRENCY = "AUD"
-TIMEZONE = "Australia/Sydney"
+SUPABASE_URL = st.secrets.get("SUPABASE_URL", os.getenv("SUPABASE_URL"))
+SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", os.getenv("SUPABASE_KEY"))
