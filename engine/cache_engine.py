1e009c875b71c4300926c5d11acf9c4121505114
Tradehub-design <2gn2d7cmnf@privaterelay.appleid.com>
2026-06-27 00:34:50 +0800

    Update cache_engine.py

--- engine/cache_engine.py
+++ engine/cache_engine.py
@@ -4,6 +4,7 @@ import streamlit as st
 from core.config import AppConfig
 from core.logger import Logger
 from engine.trade_engine import TradeEngine
+from engine.live_engine import LiveEngine
 
 
 class CacheEngine:
@@ -25,30 +26,37 @@ class CacheEngine:
     @staticmethod
     @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def trades():
-        return CacheEngine._safe_load(
-            TradeEngine.load_all
-        )
+        return CacheEngine._safe_load(TradeEngine.load_all)
 
     @staticmethod
     @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def reviews():
-        return CacheEngine._safe_load(
-            TradeEngine.load_reviews
-        )
+        return CacheEngine._safe_load(TradeEngine.load_reviews)
 
     @staticmethod
     @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def screenshots():
-        return CacheEngine._safe_load(
-            TradeEngine.load_screenshots
-        )
+        return CacheEngine._safe_load(TradeEngine.load_screenshots)
 
     @staticmethod
     @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def replays():
-        return CacheEngine._safe_load(
-            TradeEngine.load_replays
-        )
+        return CacheEngine._safe_load(TradeEngine.load_replays)
+
+    @staticmethod
+    @st.cache_data(ttl=AppConfig.CACHE_TTL)
+    def account_snapshot():
+        return CacheEngine._safe_load(LiveEngine.account_snapshot)
+
+    @staticmethod
+    @st.cache_data(ttl=AppConfig.CACHE_TTL)
+    def open_positions():
+        return CacheEngine._safe_load(LiveEngine.open_positions)
+
+    @staticmethod
+    @st.cache_data(ttl=AppConfig.CACHE_TTL)
+    def sync_status():
+        return CacheEngine._safe_load(LiveEngine.sync_status)
 
     @staticmethod
     def clear():
