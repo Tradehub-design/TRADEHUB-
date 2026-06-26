97f8113e66c219c75b3f73f87aede83f54b152bb
Tradehub-design <2gn2d7cmnf@privaterelay.appleid.com>
2026-06-26 23:58:36 +0800

    Update cache_engine.py

--- engine/cache_engine.py
+++ engine/cache_engine.py
@@ -1,50 +1,55 @@
+import pandas as pd
 import streamlit as st
 
 from core.config import AppConfig
-from core.error_handler import ErrorHandler
-
+from core.logger import Logger
 from engine.trade_engine import TradeEngine
 
 
 class CacheEngine:
 
     @staticmethod
+    def _safe_load(loader):
+        try:
+            result = loader()
+
+            if result is None:
+                return pd.DataFrame()
+
+            return result
+
+        except Exception as e:
+            Logger.exception(str(e))
+            return pd.DataFrame()
+
+    @staticmethod
     @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def trades():
-
-        return ErrorHandler.execute(
-            TradeEngine.load_all,
-            default=None
+        return CacheEngine._safe_load(
+            TradeEngine.load_all
         )
 
     @staticmethod
     @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def reviews():
-
-        return ErrorHandler.execute(
-            TradeEngine.load_reviews,
-            default=None
+        return CacheEngine._safe_load(
+            TradeEngine.load_reviews
         )
 
     @staticmethod
     @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def screenshots():
-
-        return ErrorHandler.execute(
-            TradeEngine.load_screenshots,
-            default=None
+        return CacheEngine._safe_load(
+            TradeEngine.load_screenshots
         )
 
     @staticmethod
     @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def replays():
-
-        return ErrorHandler.execute(
-            TradeEngine.load_replays,
-            default=None
+        return CacheEngine._safe_load(
+            TradeEngine.load_replays
         )
 
     @staticmethod
     def clear():
-
         st.cache_data.clear()
