37184acf7c2964bab9f82e49c8d348881fbcb31d
Tradehub-design <2gn2d7cmnf@privaterelay.appleid.com>
2026-06-26 21:44:07 +0800

    Update cache_engine.py

--- engine/cache_engine.py
+++ engine/cache_engine.py
@@ -1,26 +1,32 @@
 import streamlit as st
 
+from core.config import AppConfig
+
 from engine.trade_engine import TradeEngine
 
 
 class CacheEngine:
 
     @staticmethod
-    @st.cache_data(ttl=60)
+    @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def trades():
         return TradeEngine.load_all()
 
     @staticmethod
-    @st.cache_data(ttl=60)
+    @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def reviews():
         return TradeEngine.load_reviews()
 
     @staticmethod
-    @st.cache_data(ttl=60)
+    @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def screenshots():
         return TradeEngine.load_screenshots()
 
     @staticmethod
-    @st.cache_data(ttl=60)
+    @st.cache_data(ttl=AppConfig.CACHE_TTL)
     def replays():
         return TradeEngine.load_replays()
+
+    @staticmethod
+    def clear():
+        st.cache_data.clear()
