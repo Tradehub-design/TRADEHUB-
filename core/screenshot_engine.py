4488a7e02c1468ce4f03764a0266910609da0046
Tradehub-design <2gn2d7cmnf@privaterelay.appleid.com>
2026-06-27 00:07:04 +0800

    Update screenshot_engine.py

--- core/screenshot_engine.py
+++ core/screenshot_engine.py
@@ -4,11 +4,28 @@ import time
 class ScreenshotEngine:
 
     @staticmethod
+    def clean_text(value):
+        if value is None:
+            return "unknown"
+
+        value = str(value)
+        value = value.replace(" ", "_")
+        value = value.replace("/", "_")
+        value = value.replace("\\", "_")
+        value = value.replace(":", "_")
+        value = value.replace("|", "_")
+
+        return value
+
+    @staticmethod
     def build_file_path(trade_ticket, account_number, screenshot_type, filename):
-        safe_filename = filename.replace(" ", "_")
+        safe_filename = ScreenshotEngine.clean_text(filename)
+        safe_type = ScreenshotEngine.clean_text(screenshot_type)
+        safe_account = ScreenshotEngine.clean_text(account_number)
+        safe_ticket = ScreenshotEngine.clean_text(trade_ticket)
         timestamp = int(time.time())
 
-        return f"{account_number}/{trade_ticket}/{screenshot_type}_{timestamp}_{safe_filename}"
+        return f"{safe_account}/{safe_ticket}/{safe_type}_{timestamp}_{safe_filename}"
 
     @staticmethod
     def create_payload(
