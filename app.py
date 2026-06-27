3ede2cbc5fca92a3f4aa61222b78588bd3e7c4c2
Tradehub-design <2gn2d7cmnf@privaterelay.appleid.com>
2026-06-27 15:14:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from v2.navigation import V2Navigation
 from v2.style import V2Style
 from v2.dashboard import DashboardV2
+from v2.trade_review import TradeReviewV2
 
 
 st.set_page_config(
@@ -18,6 +19,8 @@ selected = V2Navigation.sidebar()
 
 if selected == "Dashboard":
     DashboardV2.render()
+elif selected == "Trade Review":
+    TradeReviewV2.render()
 else:
     st.title(selected)
     st.info("This V2 workspace will be built next.")
